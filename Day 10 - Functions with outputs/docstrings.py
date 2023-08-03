6bb645531881efcd120966040d01dd53ae299661
Ateeth <ateetharun@gmail.com>
2023-08-03 18:05:47 +0300

    Day 10

---
+++ Day 10 - Functions with outputs/docstrings.py
@@ -0,0 +1,13 @@
+def format_name(f_name, l_name):
+    """
+    Take a first name and last name and format it to return the title case version of the name
+    """
+    formatted_f_name = f_name.title()
+    formatted_l_name = l_name.title()
+
+    return f"{formatted_f_name} {formatted_l_name}"
+
+
+formatted_name = format_name("ateeth", "arun")
+
+print(formatted_name)
